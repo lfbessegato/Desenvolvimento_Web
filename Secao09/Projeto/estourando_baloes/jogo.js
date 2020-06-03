cb3d2ecd0e7aa1e2e6abf6aeac2078f3a3a0a7fb
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-03 08:42:47 -0300

    Secao09 - Projeto Final (estourando baloes) - fix - Impedir estouro dos baloes apos vitoria ou game over

--- Secao09/Projeto/estourando_baloes/jogo.js
+++ Secao09/Projeto/estourando_baloes/jogo.js
@@ -52,7 +52,9 @@ function contagem_tempo(segundos){
 }
 
 function game_over(){
-	alert('Fim de jogo, você não conseguiu estourar todos os balões a tempo');
+	remove_eventos_baloes();
+    alert('Fim de jogo, você não conseguiu estourar todos os balões a tempo');
+	situacao_jogo();
 }
 
 function cria_baloes(qtde_baloes){
@@ -108,4 +110,15 @@ function situacao_jogo(baloes_inteiros){
 
 function parar_jogo(){
 	clearTimeout(timerId);
+}
+
+function remove_eventos_baloes() {
+    var i = 1; //contado para recuperar balões por id
+    
+    //percorre o lementos de acordo com o id e só irá sair do laço quando não houver correspondência com elemento
+    while(document.getElementById('b'+i)) {
+        //retira o evento onclick do elemnto
+        document.getElementById('b'+i).onclick = '';
+        i++; //faz a iteração da variávei i
+    }
 }
