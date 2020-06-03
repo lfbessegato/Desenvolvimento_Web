db9954ab35849d06016b4ab676cf529c7e39dc80
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-03 13:26:02 -0300

    Secao10 - Projeto - Jogo da Velha removendo eventos

--- Secao10/Projeto/jogo_da_velha/jogo.js
+++ Secao10/Projeto/jogo_da_velha/jogo.js
@@ -45,6 +45,7 @@ $(document).ready( function(){
 	$('.jogada').click( function(){
 
 		var id_campo_clicado = this.id;
+		$('#'+id_campo_clicado).off() // destiva a opção de clique
 		jogada(id_campo_clicado);
 	});
 
