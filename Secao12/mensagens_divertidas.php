11a6d21737b2f1c9df0cfa94e12980d0fd81f1c8
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 05:03:37 -0300

    Secao12 - PHP - Aula 155 - echo-print

---
+++ Secao12/mensagens_divertidas.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>Mensagens divertidas</title>
+	</head>
+
+	<body>
+		<?php
+			echo 'Teste tag padrão<br />';
+			print 'Teste de impressão com o print<br />';
+		?>
+		
+		<?='Teste tag impressão'?>
+		
+	</body>
+</html>
