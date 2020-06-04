189202f85a577425de01465fe46b2177e2336abf
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 07:17:46 -0300

    Secao12 - PHP - Aula 170 - switch

---
+++ Secao12/switch.php
@@ -0,0 +1,17 @@
+<? 
+
+    $opcao = 1;    
+    switch($opcao){
+        case 1:
+            echo 'primeiro case';
+            break;
+        case 2:
+            echo 'segundo case';
+            break;
+        default:
+            echo 'nenhuma opção';
+            break;
+
+    }
+
+?>
