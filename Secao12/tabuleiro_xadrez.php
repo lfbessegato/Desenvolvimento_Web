e717ca586fe5f865607d6a828f439125be744b96
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 06:02:08 -0300

    Secao12 - PHP - Aula 160 - matriz

---
+++ Secao12/tabuleiro_xadrez.php
@@ -0,0 +1,27 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+		<title>tabuleiro</title>
+	</head>
+
+	<body>
+		<?php
+			//matriz
+			$tabuleiro[8]['a'] = 'torre preta';
+			$tabuleiro[7]['b'] = 'bispo preta';
+			$tabuleiro[6]['c'] = 'cavalo preta';
+			$tabuleiro[5]['d'] = 'rainha preta';
+			$tabuleiro[4]['e'] = 'rei preta';
+			$tabuleiro[3]['f'] = 'bispo preta';
+			$tabuleiro[2]['g'] = 'cavalo preta';
+			$tabuleiro[1]['h'] = 'torre preta';
+
+			var_dump($tabuleiro);
+
+		?>
+		
+		
+	</body>
+</html>
