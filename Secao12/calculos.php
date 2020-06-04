49959c3a01bbfe52bd064eedfccc0020cc8dcbd5
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 07:32:24 -0300

    Secao12 - PHP - Aula 172 - efetuando calculos matematicos

---
+++ Secao12/calculos.php
@@ -0,0 +1,32 @@
+<?php 
+
+    // operações matemáticas
+    // + adição 
+    // - subtração
+    // / divisão
+    // * multiplicação
+    // % mod
+
+    $n1 = 7;
+    $n2 = 3;
+
+    //adição
+    echo $n1 + $n2;
+    echo '<br />';
+
+    //subtracao
+    echo $n1 - $n2;
+    echo '<br />';
+
+    //Divisão
+    echo $n1 / $n2;
+    echo '<br />';
+
+    // Multiplicação
+    echo $n1 * $n2;
+    echo '<br />';
+
+    //Mod
+    echo $n1 % $n2;
+
+?>
