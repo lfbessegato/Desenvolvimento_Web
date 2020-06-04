64224207fd85c8b7ecfff66d7b625f893f7df4fb
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 06:52:30 -0300

    Secao12 - PHP - Aula 167 - if_else parte 1

---
+++ Secao12/if_else.php
@@ -0,0 +1,19 @@
+<?php
+
+    // operadores
+    // == (Igual)
+    // === (idêntico)
+    // != ou <> (diferente)
+    // !== (não identico)
+    // < (menor)
+    // > (maior)
+    // <= (menor igual)
+    // >= (maior igual)
+
+    if(true) {
+        echo 'sim, expressão é verdadeira';
+    } else {
+        echo 'não, expressão é falsa';
+    }
+
+?>
