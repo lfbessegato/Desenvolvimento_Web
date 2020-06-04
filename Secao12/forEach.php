8a905be453f01e0fdeb47c47ac9449cb59ddad88
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-04 08:35:29 -0300

    Secao12 - PHP - Aula 178 - efetuando repeticao: forEach

---
+++ Secao12/forEach.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $listas_compras[1] = 'Arroz';
+    $listas_compras[2] = 'Banana';
+    $listas_compras[3] = 'Morango';
+    $listas_compras[4] = 'Feijão';
+
+    foreach($listas_compras as $i){
+
+        echo $i.'<br />';
+    }
+
+
+?>
