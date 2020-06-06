081441f37ae34a5bc41bca3fda634459ef56b571
Luciano Bessegato <lfrbessegato@gmail.com>
2020-06-06 14:36:17 -0300

    Secao17 - Projeto Twitter Clone - Adicionando o botão para seguir pessoas

--- Secao17/twitter_clone/get_pessoas.php
+++ Secao17/twitter_clone/get_pessoas.php
@@ -26,6 +26,14 @@
 
                 echo '<strong>' . $registro['usuario'] . '</strong> <small> - ' . $registro['email'] . ' </small>';
 
+                echo '<p class="list-group-item-text pull-right">';
+
+                    echo '<button type="button" class="btn btn-default">Seguir</button>';
+                
+                echo '</p>';
+
+                echo '<div class="clearfix"></div>';
+
             echo '</a>';
 
         }
